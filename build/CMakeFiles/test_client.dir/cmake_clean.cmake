64d2b29a696590c4bfbbb212a0b580c7015d8edb
taosiming <taosiming@fancyjing.com>
2022-09-29 09:53:31 +0000

    test

---
+++ build/CMakeFiles/test_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_client.dir/main/main.cpp.o"
+  "CMakeFiles/test_client.dir/main/server.cpp.o"
+  "test_client"
+  "test_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
